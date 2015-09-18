ece3bb22b1b052162715ada3371fa711fd0d8147
mariahammerstrom <maria.hammerstrom@astro.uio.no>
2015-09-18 11:19:18 +0200

    Added armadillo functionality.

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -1,10 +1,21 @@
+// PROJECT 2
+// Schrödinger's equation for two electrons in a threedimensional harmonic oscillator well
+
 #include <iostream>
+#include <cstdlib>
+#include <time.h>
+#include <math.h>
+#include <fstream>
+#include <armadillo>
 
 using namespace std;
+using namespace arma;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int N = 10;
+
+    mat A(N,N);
     return 0;
 }
 
